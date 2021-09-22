2a3f1f28cf2ce01ecb360460bcd4f2f372686881
MadosMark <69796358+MadosMark@users.noreply.github.com>
2021-09-22 13:53:50 +0200

    Added 404 error page

--- rudolf/single.php
+++ rudolf/single.php
@@ -5,9 +5,9 @@
     <?php while (have_posts()) : the_post(); ?>
 
         <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-
         <?php the_content(); ?>
-        <?php the_date() ?>
+        <?php the_date() ?> <br>
+        <?php the_post_thumbnail('medium') ?>
     <?php endwhile; ?>
 
 <?php endif; ?>
