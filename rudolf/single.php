d0dee35f43c02a0adbc0ec2b8213275e265ca710
MadosMark <69796358+MadosMark@users.noreply.github.com>
2021-09-22 11:50:15 +0200

    Added new post with Contact info

--- rudolf/single.php
+++ rudolf/single.php
@@ -7,12 +7,8 @@
         <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
 
         <?php the_content(); ?>
-
+        <?php the_date() ?>
     <?php endwhile; ?>
-    <?php $posts = get_posts() ?>
-    <?php foreach ($posts as $post) : ?>
-        <h2> <?= $post->title ?></h2>
-    <?php endforeach ?>
 
 <?php endif; ?>
 
