3eae8fba12e6cec2cd162dfb8c73f64535c7cb93
MadosMark <69796358+MadosMark@users.noreply.github.com>
2021-09-22 11:06:04 +0200

    New About page added and CSS connected

--- rudolf/header.php
+++ rudolf/header.php
@@ -1,6 +1,11 @@
-<?php $menuItems = wp_get_nav_menu_items('huvudmeny'); ?>
+<header>
 
-<nav>
+    <?php wp_head(); ?>
+    <?php $menuItems = wp_get_nav_menu_items('huvudmeny'); ?>
+
+</header>
+
+<nav class="nav-bar">
     <?php foreach ($menuItems as $item) : ?>
         <a class="<?= $item->object_id == $currentPageId ? 'active' : '' ?> " href="<?= $item->url; ?>"><?= $item->title; ?></a>
     <?php endforeach; ?>
