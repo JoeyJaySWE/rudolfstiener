525d82aab7930dda1b8b6b9c1df94bb204318c65
JoeyJaySWE <joeyjay@joeyjaydigital.com>
2021-09-22 12:28:09 +0200

    nav fix + debug prep
    fixed the isssue with our navigation not showing which page you were on and prepped some files for debugging.

--- rudolf/functions.php
+++ rudolf/functions.php
@@ -1,7 +1,7 @@
 <?php
 
 declare(strict_types=1);
-
+// var_dump("functions.php");
 
 add_action('wp_enqueue_scripts', function () {
     wp_enqueue_style('style', get_template_directory_uri() . '/style.css');
